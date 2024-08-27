312620100365b6c6c16cb6d19b6d83a3f4749c64
Joseph Johnson <82980802+josephtjohnson@users.noreply.github.com>
2024-08-27 19:00:38 -0400

    Update HW2.java

--- Week 2/HW2.java
+++ Week 2/HW2.java
@@ -1,28 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package HW2;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+/**
+ *
+ * @author Johns
+ */
 class StringProcessor
 {
   public static String processStringInput(String str) 
   {
     //Create a new stack
-    Stack<string> stack = new Stack<>(str.length + );
-    string result;
+    Stack<Character> stack = new Stack<>();
+    
+    String reverse = "";
+    String result = "";
     
     //add each character onto the stack
-    foreach (char c in str)
+    for (int i = 0; i < str.length(); i++)
     {
-      //what happens if they start with a backspace?
-      if (c == "<" && !stack.empty())
+      if (str.charAt(i) == '<')
       {
-        stack.pop();
+        if (!stack.empty())
+        {
+            stack.pop();
+        }
       }
       else
       {
-        stack.push(c);
+        stack.push(str.charAt(i));
       }
       
     }
-    for (int i = 0; i < stack.size(); i++)
+    for (int i = 0; !stack.isEmpty(); i++)
     {
-      result += stack[i];
+      reverse += stack.pop();
+    }
+    for (int i = reverse.length() - 1; i >= 0; i--)
+    {
+        result += reverse.charAt(i);
     }
 
     return result;
@@ -30,29 +51,28 @@ class StringProcessor
 
   public static boolean isPalindrome(String str)
   {
-    bool isPalindrome = true;
+    Boolean isPalindrome = true;
 
     //Create a new stack
-    Stack<string> stack = new Stack<>(str.length + );
-    string result;
+    Stack<Character> stack = new Stack<>();
 
-    int i, mid = str.length / 2;
+    int i, mid = str.length() / 2;
 
     for (i = 0; i < mid; i++)  
     {
-      stack.push(str[i]);
+      stack.push(str.charAt(i));
     }
 
-    if (length % 2 != 0) 
+    if (str.length() % 2 != 0) 
     {
         i++;
     }
 
-    while (i < str.length)
+    while (i < str.length())
     {
       char c = stack.pop();
 
-      if (c != str[i])
+      if (c != str.charAt(i))
       {
         isPalindrome = false;
         return isPalindrome;
@@ -66,15 +86,15 @@ class StringProcessor
   {
 
     //Create a new stack
-    Stack<string> stack = new Stack<>(str.length + );
-    int result;
+    Stack<Character> stack = new Stack<>();
+    int result = 0;
     
     int i;
-    while (stack.empty)
+    for (i = 0; i < str.length() - 1; i++) //what happens if the input has no zeros or ones?
     {
-      if (str[i] == "0" || str[i] == "1")
+      if (str.charAt(i) == '0' || str.charAt(i) == '1')
       {
-        stack.push(str[i]);
+        stack.push(str.charAt(i));
       }
       else
       {
@@ -82,29 +102,29 @@ class StringProcessor
       }
     }
 
-    if (!stack.empty)
+    if (!stack.empty())
     {
     char firstEncounter = stack.peek();
-      for(int j = i + 1; j < str.length; j++)
+      for(int j = i + 1; j < str.length(); j++)
       {
-        if (firstEncounter == "0")
+        if (firstEncounter == '0')
         {
-          if (str[j] == "0")
+          if (str.charAt(j) == '0')
           {
-            stack.push(str[j]);
+            stack.push(str.charAt(j));
           }
-          else if (str[j] == "1")
+          else if (str.charAt(j) == '1')
           {
             stack.pop();
           }
         }
-        if (firstEncounter == "1")
+        if (firstEncounter == '1')
         {
-          if (str[j] == "1")
+          if (str.charAt(j) == '1')
           {
-            stack.push(str[j]);
+            stack.push(str.charAt(j));
           }
-          else if (str[j] == "0")
+          else if (str.charAt(j) == '0')
           {
             stack.pop();
           }
@@ -112,15 +132,15 @@ class StringProcessor
       }
       if (stack.empty())
       {
-        result = 0
+        result = 0;
       }
-      if (sneak.peek() == "0")
+      if (stack.peek() == '0')
       {
-        result = -1
+        result = -1;
       }
-      else if (sneak.peek() == "1")
+      else if (stack.peek() == '1')
       {
-        result = 1
+        result = 1;
       }
     }
     else
@@ -133,34 +153,32 @@ class StringProcessor
   public static void main(String[] args) 
   {
     Scanner scanner = new Scanner(System.in);  //Create a Scanner object
-    int quitCode;
     do
     {
-      System.out.println("Enter a string (< for backspace or "q" to stop the program): ");
+      System.out.println("Enter a string (< for backspace or 'q' to stop the program): ");
       String userInput = scanner.nextLine();  //Read user input
 
-      if (userInput == "q")
+      if ("q".equals(userInput))
       {
         System.out.println("Thanks for using this program.");
-        quitCode = 0;
         break;
       }
-      if (userInput is not null or empty)
+      if (userInput != null && !userInput.isEmpty() && !userInput.isBlank())
       {
         //process user input
-        string processedUserInput = processStringInput(userInput);
+        String processedUserInput = processStringInput(userInput);
 
-        System.out.println(string.format("After processing backspaces, the string becomes: {0}", processedUserInput));
+        System.out.println("After processing backspaces, the string becomes: " + processedUserInput);
       
         //check if palindrome
-        bool isPalindrome = isPalindrome(processedUserInput);
+        Boolean isPalindrome = isPalindrome(processedUserInput);
         if (isPalindrome)
         {
-          System.out.println(string.format("{0} is a palindrome",processedUserInput));
+          System.out.println(processedUserInput + " is a palindrome");
         }
         else
         {
-          System.out.println(string.format("{0} is not a palindrome",processedUserInput));
+          System.out.println(processedUserInput + " is not a palindrome");
         }
   
         //check zeros and ones
@@ -182,8 +200,6 @@ class StringProcessor
       {
         System.out.println("You must type something! Try again.");
       }
-    } while (quitCode != 0);
-
-  
+    } while (true);
   }
 }
