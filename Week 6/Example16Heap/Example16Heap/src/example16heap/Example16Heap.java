10415930d01d76dfd0dc995f6f1d0e40d7cb94f8
Joseph Johnson <82980802+josephtjohnson@users.noreply.github.com>
2024-09-27 10:25:49 -0400

    Update Example16Heap.java

--- Week 6/Example16Heap/Example16Heap/src/example16heap/Example16Heap.java
+++ Week 6/Example16Heap/Example16Heap/src/example16heap/Example16Heap.java
@@ -15,23 +15,48 @@ import java.util.Scanner;
 public class Example16Heap 
 {
     public static void main(String[] args) 
-    {
+    {   
+	Heap h = new Heap(25);
         Scanner input = new Scanner(System.in);
-
-        Heap myheap = new Heap(100);
+        int option;
+        do
+        {
+            System.out.println("Select from:\n1. Read items and build heap\n2. Display heap\n3. Insert a node\n4. Remove the largest node\n5. Search for a key\n0. Exit");
+            option = input.nextInt();
+            switch (option)
+            {
+                case 1:
+                    h.build(); //done I think
+                    break;
+                case 2:
+                    h.print(); //done I think
+                    break;
+                case 3:
+                    h.insert();
+                    break;
+                case 4:
+                    h.remove();
+                    break;
+	        case 5:
+		    h.find();
+		    break;
+                case 0:
+                    break;
+                default:
+                    System.out.println("Invalid option!  Try again: ");
+            }
+            
+        } while (option != 0);
         
-        // heapsort
-        System.out.print("The sorted list: ");
-	while (!myheap.isEmpty())		
-            System.out.print(myheap.remove() + " ");
-        System.out.println();
+        System.out.println("Thanks for using my program.");
+                
     }
     
 }
 
 /**
  * 
- * Assume all items in the heap are non-negative
+ * Assume all items in the heap are non-negative and distinct
  */
 class Heap
 {
@@ -48,25 +73,85 @@ class Heap
 
     public void build()
     {
-        // Store a set of data items into the array. They will make a heap without any adjustment
-        a[0] = 95;
-        a[1] = 81;
-        a[2] = 72;
-        a[3] = 39;
-        a[4] = 53;
-        a[5] = 71;
-        a[6] = 23;
-        a[7] = 38;
-        a[8] = 34;
-        a[9] = 6;
-        n = 10;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter up to 25 elements (negative to end): ");
         
-        // In the homework assignment, need to remove the above 11 statements and replace them with your own code that
-        // 1) ask the user to enter non-negative intergers (negative to stop)
-        // 2) use the entered non-negative integers to build a heap using the algorithm discussed in class
-        
+        int i;
+        for (i = 0; i < h.length; i++)        // allow at most a.length elements to be stored into the heap
+        {
+            int e = input.nextInt();
+            
+            if (e >= 0)
+                h[i] = e;                   // store a non-negative number into the array 
+            else                        
+                break;
+        }
+        n = i;                              // record the total number of elements that have been stored into the heap
+
+	startIndex = (n / 2) - 1;
+
+	for (int i = startIndex; i >= 0; i--)
+	     heapify(startIndex);
     }
-    
+
+    public void heapify(int i)
+    {
+	int largest = i;
+	int left = 2 * i + 1;
+	int right = 2 * i + 2;
+
+	if (l < n && h[left] > h[largest])
+	     largest = l;
+	if (r < n && h[right] > h[largest])
+	     largest = r;
+
+    	if (largest != i)
+	{
+	     int temp = h[i];
+	     h[i] = h[largest];
+	     h[largest] = swap;
+
+	     heapify(largest);
+	}
+    }
+
+    public void print()
+    {
+	System.out.println("Heap:");
+	for (int i = 0; i < n; i++)
+	{
+	     for (int j = 0; j < Math.pow(2,i) && j + Math.pow(2,i) < n; j++)
+	     {
+	          System.out.print(h[j+(int)Math.pow(2,i)-1];
+	     }
+	     System.out.println();
+	}
+    }
+
+    public void insert(int i)
+	{
+		Scanner input = new Scanner(System.in);
+        	System.out.print("Enter a non-negative integer to be inserted (negative to end): ");
+		
+		if (h.length) < n)
+		{
+			h[n] = i;
+		}
+		int i = n;
+		while (i > 0)
+		{
+			int parentIndex = (i-1)/2
+			if (h[n] > h[parent])
+			{
+			
+				int temp = h[parent];
+				h[parent] - h[n];
+				h[n] = temp;
+				i = parentIndex;
+			}
+		}
+	}
+	
     public boolean isEmpty()
     {
         return (n == 0);
@@ -125,5 +210,10 @@ class Heap
         else					// right child is larger
             return 2 * i + 2;
     }
+
+	public void findKey(int k)
+	{
+		
+	}
 }
 
